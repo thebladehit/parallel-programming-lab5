f9ad0089cd3c750e3d6d1230b995c4ce4d1c40a7
thebladehit <boqdanyarmolka@gmail.com>
2025-05-14 11:51:00 +0300

    Rename vector and matrix type names

--- data/data.ads
+++ data/data.ads
@@ -13,17 +13,17 @@ package Data is
   P: Integer := 8;
   H: Integer := N / P;
 
-  type VGeneral is array(Integer range <>) of Integer;
-    subtype V is VGeneral(1..N);
-    subtype V1H is VGeneral(1..H);
-    subtype V2H is VGeneral(1..2*H);
-    subtype V3H is VGeneral(1..3*H);
-    subtype V4H is VGeneral(1..4*H);
+  type VectorGeneral is array(Integer range <>) of Integer;
+    subtype Vector is VectorGeneral(1..N);
+    subtype Vector1H is VectorGeneral(1..H);
+    subtype Vector2H is VectorGeneral(1..2*H);
+    subtype Vector3H is VectorGeneral(1..3*H);
+    subtype Vector4H is VectorGeneral(1..4*H);
 
-  type MGeneral is array(Integer range <>, Integer range <>) of Integer;
-    subtype M is MGeneral(1..N);
-    subtype M1H is MGeneral(1..H);
-    subtype M2H is MGeneral(1..2*H);
-    subtype M3H is MGeneral(1..3*H);
-    subtype M4H is MGeneral(1..4*H);
+  type MatrixGeneral is array(Integer range <>, Integer range <>) of Integer;
+    subtype Matrix is MatrixGeneral(1..N, 1..N);
+    subtype Matrix1H is MatrixGeneral(1..N, 1..H);
+    subtype Matrix2H is MatrixGeneral(1..N, 1..2*H);
+    subtype Matrix3H is MatrixGeneral(1..N, 1..3*H);
+    subtype Matrix4H is MatrixGeneral(1..N, 1..4*H);
 end Data;
